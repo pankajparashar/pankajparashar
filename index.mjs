77523a89df35f6a2a91b9c8fda31a23702094909
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-06-10 18:31:46 +0530

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import scrape from "website-scraper";
+import PuppeteerPlugin from "website-scraper-puppeteer";
+
+import fs from "fs"
+
+scrape({
+    urls: ["https://bento.me/pankajparashar"],
+    directory: "public",
+    plugins: [
+        new PuppeteerPlugin({
+            launchOptions: {
+                headless: true,
+                defaultViewport: {
+                    width: 1920,
+                    height: 1080,
+                },
+            },
+            blockNavigation: true,
+        }),
+    ],
+}).then(() => {
+    console.log("done");
+    fs.rmdirSync("public/js", { recursive: true, force: true });
+});
